cd830c6e70aa7f6e31103dc7723f60f373dee108
Lucas Mendes <lucas.men195@gmail.com>
2013-03-03 04:06:04 -0300

    Estrutura de blog criado utilizando rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'eb39b7686aeadf3924cbb503e5853a1a599270ac830d6dd7adb5d254ad6eebdb6027880530fbcb4b60f5f4f0329a3a690eede07796d3022457f01cb86bc0a17b'
